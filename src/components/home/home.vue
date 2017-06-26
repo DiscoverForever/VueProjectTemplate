61ebf52bbefc2f6df57909e3473f24979caac276
tangyy <tangyueyang@lztogether.com>
2017-06-26 16:50:17 +0800

    Mod Test Leancloud CRUD

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,8 +1,64 @@
 <template>
-    <div class="home">home</div>
+  <div class="home">home</div>
 </template>
 <script>
-    export default {};
+  import Test, {TestQuery} from './test.ex';
+  export default {
+    created() {
+      this.create();
+      this.query();
+      this.update();
+      this.delete();
+    },
+    methods: {
+      create() {
+        let test = new Test();
+        test.name = 'tyy';
+        test.save().then((res) => {
+          console.log('Create Success');
+          console.log(res);
+        }, (e) => {
+          console.log(e);
+        });
+      },
+      update() {
+        let query = new TestQuery();
+        query.first().then((test) => {
+          test.name = 'update';
+          test.save().then((res) => {
+            console.log('Update Success');
+            console.log(res);
+          }, (e) => {
+            console.log(e);
+          });
+        }, (e) => {
+          console.error(e);
+        });
+      },
+      delete() {
+        let query = new TestQuery();
+        query.first().then((test) => {
+          test.destroy().then((res) => {
+            console.log('Delete Success');
+            console.log(res);
+          }, (e) => {
+            console.log(e);
+          });
+        }, (e) => {
+          console.error(e);
+        });
+      },
+      query() {
+        let query = new TestQuery();
+        query.find().then((res) => {
+          console.log('Query Success');
+          console.loe(res);
+        }, (e) => {
+          console.error(e);
+        });
+      }
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App';
 import router from './router';
 import Mint from 'mint-ui';
 import axios from 'common/js/axios';
+import AV from 'leancloud-storage';
 import 'mint-ui/lib/style.css';
 import 'common/stylus/index.styl';
 import store from './store';
@@ -12,6 +13,12 @@ import store from './store';
 Vue.config.productionTip = false;
 Vue.use(Mint);
 Vue.use(axios);
+const APP_ID = '0yMcke4M4YaWdl9I32MNEM7f-gzGzoHsz';
+const APP_KEY = 'J2sIDkQYOK1yeNs1JkJ4Y1d1';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 /* eslint-disable no-new */
 new Vue({
